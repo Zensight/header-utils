f3413aea5bfa1c31f4dcb6d4d767b3059261d258
Isaac Cambron <isaac@isaaccambron.com>
2015-09-02 00:22:26 -0400

    preparing to release

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject co.zensight/header-utils "0.1.0-SNAPSHOT"
+(defproject co.zensight/header-utils "0.1.0"
   :description "Tools for working HTTP headers"
   :url "http://github.com/zensight/header-utils"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.html"}
+  :deploy-repositories [["releases" :clojars]]
+  :signing {:gpg-key "isaac@isaaccambron.com"}
   :scm {:name "git"
         :url "https://github.com/Zensight/file-buffer"}
   :dependencies [[org.clojure/clojure "1.6.0"]
