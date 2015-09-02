d5850cdf9ec02630482ca45492b2f1eb417e0a6a
Isaac Cambron <isaac@isaaccambron.com>
2015-09-02 00:55:34 -0400

    fixing scm url

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject co.zensight/header-utils "0.1.0"
+(defproject co.zensight/header-utils "0.1.1"
   :description "Tools for working HTTP headers"
   :url "http://github.com/zensight/header-utils"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.html"}
   :deploy-repositories [["releases" :clojars]]
-  :signing {:gpg-key "isaac@isaaccambron.com"}
+  :signing {:gpg-key "748C1352"}
   :scm {:name "git"
-        :url "https://github.com/Zensight/file-buffer"}
+        :url "https://github.com/Zensight/header-utils"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.4.1"]])
